2320523f72cb6dc1805a90edf9f61bbecd65dbbd
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-02-22 13:06:59 +0530

    top bar added

---
+++ boot.navbar.js
@@ -0,0 +1,28 @@
+utils.define('boot.navbar').extend('spam').as(function(navbar, _instance_){
+	
+	_instance_._init_ = function(){
+		
+		//Set View
+		this.style('boot.navbar.css');
+		
+		var data = {};
+		data.nav_pos_class = this.options.position || '';
+		data.container_class = this.options.fluid ? 'container-fluid' : 'container';
+		var content_html = this.options.view || this.getClass().getPath('boot.navbar-content.html');
+		
+		var self = this;
+		self.view('boot.navbar-wrapper.html',data,false).done(function(){
+			content_html.load().done(function(html){
+				self.find('.nav-container').html(html);
+			});
+		});
+		
+	};
+	
+	_instance_._config_ = function(config){
+		this.url = config.url;
+	};
+	_instance_._get_wrapper_ = function(spam_class){
+		return $('<div class="'+spam_class+'" />');
+	};
+});
