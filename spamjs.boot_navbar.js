48e5e9d947c478f8ef7249d30beece7cdae75d6d
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-08-10 01:14:36 +0530

    added module.json and fixed

--- spamjs.boot_navbar.js
+++ spamjs.boot_navbar.js
@@ -1,28 +1,28 @@
-utils.define('spamjs.boot_navbar').extend('spamjs.module').as(function(navbar, _instance_){
+define({ 
+	name : 'spamjs.boot_navbar',
+	extend : 'spamjs.view',
+	modules : ["jsutils.file"]
+}).as(function(navbar,fileUtils){
 	
-	_instance_._init_ = function(){
+	navbar._init_ = function(){
 		
 		//Set View
-		this.style('boot.navbar.css');
-		
 		var data = {};
 		data.nav_pos_class = this.options.position || '';
 		data.container_class = this.options.fluid ? 'container-fluid' : 'container';
-		var content_html = this.options.view || this.getClass().getPath('boot.navbar-content.html');
+		var content_html = this.options.view || this.path('boot.navbar-content.html');
+		
 		
 		var self = this;
 		self.view('boot.navbar-wrapper.html',data,false).done(function(){
-			content_html.load().done(function(html){
-				self.find('.nav-container').html(html);
+			fileUtils.loadView(content_html).done(function(html){
+				self.$$.find('.nav-container').html(html);
+				//self.apply();
 			});
 		});
 		
 	};
-	
-	_instance_._config_ = function(config){
+	navbar._config_ = function(config){
 		this.url = config.url;
-	};
-	_instance_._get_wrapper_ = function(spam_class){
-		return $('<div class="'+spam_class+'" />');
 	};
 });
