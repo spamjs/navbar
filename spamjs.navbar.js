d2bdd0bbc05409d5ba32649a3242b8d7ba9eec9f
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-10-18 20:35:15 +0530

    new version

--- spamjs.navbar.js
+++ spamjs.navbar.js
@@ -1,28 +1,28 @@
-define({ 
-	name : 'spamjs.navbar',
-	extend : 'spamjs.view',
-	modules : ["jsutils.file"]
+﻿define({
+  name : 'spamjs.navbar',
+  extend : 'spamjs.view',
+  modules : ["jsutils.file"]
 }).as(function(navbar,fileUtils){
-	
-	navbar._init_ = function(){
-		
-		//Set View
-		var data = {};
-		data.nav_pos_class = this.options.position || '';
-		data.container_class = this.options.fluid ? 'container-fluid' : 'container';
-		var content_html = this.options.view || this.path('boot.navbar-content.html');
-		
-		
-		var self = this;
-		self.view('boot.navbar-wrapper.html',data,false).done(function(){
-			fileUtils.loadView(content_html).done(function(html){
-				self.$$.find('.nav-container').html(html);
-				//self.apply();
-			});
-		});
-		
-	};
-	navbar._config_ = function(config){
-		this.url = config.url;
-	};
+
+  navbar._init_ = function(){
+
+    //Set View
+    var data = {};
+    data.nav_pos_class = this.options.position || '';
+    data.container_class = this.options.fluid ? 'container-fluid' : 'container';
+    var content_html = this.options.view || this.path('boot.navbar-content.html');
+
+
+    var self = this;
+    self.$$.loadTemplate(self.path('boot.navbar-wrapper.html'),data,false).done(function(){
+      fileUtils.loadView(content_html).done(function(resp){
+        self.$$.find('.nav-container').html(resp.html);
+        //self.apply();
+      });
+    });
+
+  };
+  navbar._config_ = function(config){
+    this.url = config.url;
+  };
 });
