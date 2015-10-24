7dd1b69aabf1ec7c7b6397e0bfe7027f2f206dde
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-10-25 01:47:24 +0530

    Can pass data now

--- spamjs.navbar.js
+++ spamjs.navbar.js
@@ -1,28 +1,27 @@
 ﻿define({
-  name : 'spamjs.navbar',
-  extend : 'spamjs.view',
-  modules : ["jsutils.file"]
-}).as(function(navbar,fileUtils){
+  name: 'spamjs.navbar',
+  extend: 'spamjs.view',
+  modules: ["jsutils.file"]
+}).as(function (navbar, fileUtils) {
 
-  navbar._init_ = function(){
+  navbar._init_ = function () {
 
     //Set View
     var data = {};
     data.nav_pos_class = this.options.position || '';
     data.container_class = this.options.fluid ? 'container-fluid' : 'container';
-    var content_html = this.options.view || this.path('boot.navbar-content.html');
-
+    var content_html = this.options.view || this.options.view || this.path('boot.navbar-content.html');
 
     var self = this;
-    self.$$.loadTemplate(self.path('boot.navbar-wrapper.html'),data,false).done(function(){
-      fileUtils.loadView(content_html).done(function(resp){
+    self.$$.loadTemplate(self.path('boot.navbar-wrapper.html'), data, false).done(function () {
+      fileUtils.loadView(content_html, self.options.data).done(function (resp) {
         self.$$.find('.nav-container').html(resp.html);
         //self.apply();
       });
     });
 
   };
-  navbar._config_ = function(config){
+  navbar._config_ = function (config) {
     this.url = config.url;
   };
 });
